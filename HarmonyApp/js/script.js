b575b6c0db2fe71801b1bae9b89a54b2f89aa75a
ielison <ielisonsbarbosa@gmail.com>
2024-03-26 08:56:43 -0300

    melhorias no js

--- HarmonyApp/js/script.js
+++ HarmonyApp/js/script.js
@@ -2,31 +2,55 @@ const startBtnEl = document.querySelector(".start");
 const pauseBtnEl = document.createElement("button");
 pauseBtnEl.innerHTML =
   '<ion-icon class="pause-icon" name="pause-outline"></ion-icon>';
-  const stopBtnEl = document.createElement("button");
+const stopBtnEl = document.createElement("button");
 pauseBtnEl.classList.add("pause");
 stopBtnEl.innerHTML =
   '<ion-icon class="stop-icon" name="stop-outline"></ion-icon>';
 stopBtnEl.classList.add("stop");
 
 const controlEl = document.getElementById("controls");
-const timeEl = document.querySelector(".time");
-const musicEl = document.querySelector(".music");
+const timeBtnEl = document.querySelector(".time");
+const musicBtnEl = document.querySelector(".music");
+
+const modalMusic = document.querySelector(".dialog-music");
+const modalTime = document.querySelector(".dialog-time");
+
+const saveSound = document.querySelector(".save-sound");
+
+timeBtnEl.onclick = function () {
+  modalTime.showModal();
+};
+
+saveSound.onclick = function () {
+  modalTime.close();
+};
+
+musicBtnEl.onclick = function () {
+  modalMusic.showModal();
+};
+
+saveSound.onclick = function () {
+  modalMusic.close();
+};
 
 const animationEls = document.querySelectorAll(".circle");
 
 startBtnEl.addEventListener("click", start);
 pauseBtnEl.addEventListener("click", pause);
 stopBtnEl.addEventListener("click", stop);
+/* timeBtnEl.addEventListener("click", timer); */
+/* musicBtnEl.addEventListener("click", sound); */
 
 function start() {
   animationEls.forEach(
     (animation) => (animation.style.animationPlayState = "running")
   );
-  timeEl.remove();
-  musicEl.remove();
+  timeBtnEl.remove();
+  musicBtnEl.remove();
   startBtnEl.remove();
   controlEl.appendChild(pauseBtnEl);
   controlEl.appendChild(stopBtnEl);
+  controlEl.classList.add("controls--second");
 }
 
 function pause() {
@@ -43,10 +67,11 @@ function stop() {
     void animation.offsetWidth;
     animation.style.animation = null;
   });
-  
+
   pauseBtnEl.remove();
   stopBtnEl.remove();
   controlEl.appendChild(startBtnEl);
-  controlEl.appendChild(timeEl);
-  controlEl.appendChild(musicEl);
+  controlEl.appendChild(timeBtnEl);
+  controlEl.appendChild(musicBtnEl);
+  controlEl.classList.remove("controls--second");
 }
---
+++ HarmonyApp/js/teste.js
@@ -0,0 +1,6 @@
+const modal = document.querySelector(".dialog-music")
+const button = document.querySelector("button")
+
+button.onclick = function () {
+  modal.showModal()
+}
